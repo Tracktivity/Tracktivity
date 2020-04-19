f6fb7d0f3fbd4334e0b02c9c0b09e575e7e3f01c
Yiheng Cen Feng <33232501+YihengCenFeng@users.noreply.github.com>
2020-04-19 13:14:55 -0400

    Log out

---
+++ Tracktivity/FeedViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FeedViewController.swift
+//  Tracktivity
+//
+//  Created by Yiheng Cen Feng on 4/19/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logout(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
