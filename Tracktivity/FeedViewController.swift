0811afca5d84755e8f405f28e8b01bcc493ca259
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-04-26 15:47:16 -0400

    Setup table view cell

--- Tracktivity/FeedViewController.swift
+++ Tracktivity/FeedViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 import Parse
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let myRefreshControl = UIRefreshControl()
 
+
+    @IBOutlet var textField: UITextField!
+    
+    
+    @IBOutlet var tableView: UITableView!
+    
+    
+    var posts = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
     }
     
     @IBAction func logout(_ sender: Any) {
@@ -26,6 +55,55 @@ class FeedViewController: UIViewController {
         let delegate = UIApplication.shared.delegate as! AppDelegate
         
         delegate.window?.rootViewController = loginViewController
+    }
+    
+    
+   
+    @IBAction func onPostButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = textField.text!
+        post["author"] = PFUser.current()
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                print("Saved!")
+            }else{
+                print("error!")
+            }
+        }
+        
+        textField.text = nil
+        self.viewDidAppear(true)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
+        let post = posts[indexPath.row]
+        
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        
+        cell.captionLabel.text = post["caption"] as! String
+        
+        
+        tableView.refreshControl = myRefreshControl
+        //tableView.reloadData()
+        self.myRefreshControl.endRefreshing()
+
+        
+        return cell
+        
+        
+       
+       
+
     }
     
     /*
---
+++ Tracktivity/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  Tracktivity
+//
+//  Created by Ashraf Khan on 4/25/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    
+    @IBOutlet var photoView: UIImageView!
+    
+    @IBOutlet var usernameLabel: UILabel!
+    @IBOutlet var captionLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
